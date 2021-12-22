4bf3b3d2ade93f25eb62813d343a3481b12a6fe2
A S U S <A S U S@LAPTOP-C5SA2S1O>
2021-12-22 13:19:56 +0700

    Hallo

---
+++ src/exproject/ExProject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exproject;
+
+/**
+ *
+ * @author A S U S
+ */
+public class ExProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
